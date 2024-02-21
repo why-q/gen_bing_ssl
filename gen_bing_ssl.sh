c01044840d5299a4c0220d92d1f78aee53864887
louis <louisborges.h@gmail.com>
2024-02-21 18:18:33 +0800

    Initial script

--- gen_bing_ssl.sh
+++ gen_bing_ssl.sh
@@ -5,4 +5,4 @@ key_path="/etc/bing/private.key"
 openssl ecparam -genkey -name prime256v1 -out $key_path
 openssl req -new -x509 -days 36500 -key $key_path -out $cert_path -subj "/CN=www.bing.com"
 chmod 777 $cert_path
-chome 777 $key_path
+chmod 777 $key_path
