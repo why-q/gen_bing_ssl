84ec231259cd8e5c910920f3b55265beb70b2103
louis <louisborges.h@gmail.com>
2024-02-21 18:30:51 +0800

    Initial script

--- gen_bing_ssl.sh
+++ gen_bing_ssl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "/etc/bing" ]; then
+  mkdir /etc/bing
+fi
+
 openssl ecparam -genkey -name prime256v1 -out /etc/bing/private.key
 openssl req -new -x509 -days 36500 -key /etc/bing/private.key -out /etc/hysteria/cert.crt -subj "/CN=www.bing.com"
 chmod 777 $cert_path
