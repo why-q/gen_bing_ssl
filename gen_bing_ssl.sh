ae7fdeb84d7b4c4cf50dde96b7e898abd4faa17b
louis <louisborges.h@gmail.com>
2024-02-21 18:13:52 +0800

    Initial script

---
+++ gen_bing_ssl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cert_path="/etc/bing/cert.crt"
+key_path="/etc/bing/private.key"
+openssl ecparam -genkey -name prime256v1 -out $key_path
+openssl req -new -x509 -days 36500 -key $key_path -out $cert_path -subj "/CN=www.bing.com"
+chmod 777 $cert_path
+chome 777 $key_path
